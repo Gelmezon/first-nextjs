787227011eeffabfd4d8d69df3610dde1599b826
feitan <caf43rer@gmail.com>
2024-04-25 14:32:42 +0800

    增加图表

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import AcmeLogo from '@/app/ui/acme-logo';
 import Image from 'next/image';
-import { fetchRevenue } from '@/app/lib/data';
 
 export default function Page() {
-  const revenue = fetchRevenue();
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
